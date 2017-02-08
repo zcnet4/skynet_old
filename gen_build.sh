67e94a9fced6738b52fffcfd9ca4f5f8ae657ec1
zcnet4 <zcnet4@gmail.com>
2017-02-08 11:57:47 +0800

    [M]更新编译脚本。review by zc.

--- gen_build.sh
+++ gen_build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-#-f "scons" 
-#-f "cmake" 
-#-D"buildtype=Official"
 
-# root target: -R "skynet_bin" 
+WORKDIR=`pwd`/..
 
-current_dir=${PWD}
-#echo $current_dir
 
-python ../build/gyp_chromium -f"scons" -D"clang=0"  -D"linux_use_gold_flags=0" -D"clang_use_chrome_plugins=0" -D"linux_use_gold_binary=0"   -D"use_glib=0" -D"use_dbus=0" -D"use_aura=1" -D"linux_use_tcmalloc=0" -D"use_gconf=0" -D"use_gio=0" -D"toolkit_views=1" -D"use_x11=0" --generator-output "$current_dir" skynet.gyp
+python ${WORKDIR}/build/gyp.py -D"component=shared_library" skynet/skynet.gyp
+BUILDTYPE=Release make -j 4 -C ${WORKDIR}/build_linux
+#BUILDTYPE=Debug make -j 4 -C ${WORKDIR}/build_linux
+
+#cp ${WORKDIR}/3rd/libstdc++.so.6.0.21 ${WORKDIR}/build_linux/${BUILDTYPE}/libstdc++.so.6.0.21
+#cp ${WORKDIR}/3rd/glog/lib/linux/libglog.so.0 ${WORKDIR}/build_linux/${BUILDTYPE}/libglog.so.0
+#cp ${WORKDIR}/3rd/gperftools-2.5/lib/linux/libunwind.so.8 ${WORKDIR}/build_linux/${BUILDTYPE}/libunwind.so.8
+#cp ${WORKDIR}/3rd/gperftools-2.5/.libs/libtcmalloc_and_profiler.so.4 ${WORKDIR}/build_linux/${BUILDTYPE}/libtcmalloc_and_profiler.so.4
+
